fcbe564e7e6a04d6ec36716c60d159bdcadd3691
rscruz <santiagocrzzz1711@gmail.com>
2024-03-05 22:33:39 -0500

    definiendo estructura del proyecto

---
+++ src/app/commercial/third-parties-managment/third-parties-managment.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ThirdPartiesManagmentRoutingModule } from './third-parties-managment-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ThirdPartiesManagmentRoutingModule
+  ]
+})
+export class ThirdPartiesManagmentModule { }
