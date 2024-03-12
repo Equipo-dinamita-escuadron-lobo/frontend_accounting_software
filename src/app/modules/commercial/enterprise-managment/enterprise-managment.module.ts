8e5f9a57226e425ccf0d96e6de666a045fd106a4
rscruz <santiagocrzzz1711@gmail.com>
2024-03-11 20:43:15 -0500

    Initial commit

---
+++ src/app/modules/commercial/enterprise-managment/enterprise-managment.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { EnterpriseManagmentRoutingModule } from './enterprise-managment-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    EnterpriseManagmentRoutingModule
+  ]
+})
+export class EnterpriseManagmentModule { }
---
+++ src/app/modules/commercial/product-managment/product-managment.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ProductManagmentRoutingModule } from './product-managment-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ProductManagmentRoutingModule
+  ]
+})
+export class ProductManagmentModule { }
