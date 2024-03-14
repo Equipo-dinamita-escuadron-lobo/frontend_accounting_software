805355379dd095c55bb2015cc32e475b40f7a3b2
SantiagoAV <agredosantiago11@gmail.com>
2024-03-14 19:08:23 +0000

    Update shared.module.ts to fix the component ubication

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { SharedRoutingModule } from './shared-routing.module';
-import { HeaderComponent } from './header/header.component';
-import { SidebarComponent } from './sidebar/sidebar.component';
-import { SidebarSelectedComponent } from './sidebar-selected/sidebar-selected.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { SidebarSelectedComponent } from './components/sidebar-selected/sidebar-selected.component';
+
 
 @NgModule({
   declarations: [
