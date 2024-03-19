1d4ac2c41cbe4875fabdcae1cc52f6426749dcbc
Adrian Torres <adctg2799@gmail.com>
2024-03-19 10:51:32 -0500

    Agrega datatables, agrega componentes para crear y listar terceros

---
+++ src/app/modules/commercial/third-parties-managment/components/thirds-list/thirds-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-thirds-list',
+  templateUrl: './thirds-list.component.html',
+  styleUrl: './thirds-list.component.css'
+})
+export class ThirdsListComponent {
+
+}
