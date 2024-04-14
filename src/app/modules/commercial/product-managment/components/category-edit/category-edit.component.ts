d7151cedbf08e8abd139fcce4ac5ab2a138ab300
Jamesss73 <jamesmunozs@unicauca.edu.co>
2024-04-14 13:24:50 -0500

    added implement edit category component

--- src/app/modules/commercial/product-managment/components/category-edit/category-edit.component.ts
+++ src/app/modules/commercial/product-managment/components/category-edit/category-edit.component.ts
@@ -1,10 +1,70 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { CategoryService } from '../../services/category.service';
+import { Category } from '../../models/Category';
 
 @Component({
   selector: 'app-category-edit',
   templateUrl: './category-edit.component.html',
   styleUrl: './category-edit.component.css'
 })
-export class CategoryEditComponent {
+export class CategoryEditComponent implements OnInit{
+categoryId: string = '';
+category: Category = {} as Category;
+editForm: FormGroup;
 
+constructor(
+  private route: ActivatedRoute,
+  private categoryService: CategoryService,
+  private formBuilder: FormBuilder
+) {
+  this.editForm = this.formBuilder.group({
+    name: ['', Validators.required],
+    description: ['', Validators.required],
+    inventory: ['', Validators.required],
+    cost: ['', Validators.required],
+    sale: ['', Validators.required],
+    return: ['', Validators.required]
+  });
+}
+  ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      this.categoryId = params['id'];
+      this.getCategoryDetails();
+    });
+  }
+
+  getCategoryDetails(): void {
+    this.categoryService.getCategoryById(this.categoryId).subscribe(
+      (category: Category) => {
+        this.category = category;
+        this.editForm.patchValue({
+          name: category.name,
+          description: category.description,
+          inventory: category.inventory,
+          cost: category.cost,
+          sale: category.sale,
+          return: category.return
+        });
+      },
+      error => {
+        console.error('Error getting category details: ', error);
+      }
+    );
+    console.log('Category details: ', this.category);
+  }
+
+  onSubmit(): void {
+    if (this.editForm.valid) {
+      this.categoryService.updateCategory(this.categoryId, this.editForm.value).subscribe(
+        (category: Category) => {
+          console.log('Category updated successfully: ', category);
+        },
+        error => {
+          console.error('Error updating category: ', error);
+        }
+      );
+    }
+  }
 }
--- src/app/modules/commercial/product-managment/product-managment-routing.module.ts
+++ src/app/modules/commercial/product-managment/product-managment-routing.module.ts
@@ -8,13 +8,14 @@ import { CategoryCreationComponent } from './components/category-creation/catego
 import { UnitOfMeasureListComponent } from './components/unit-of-measure-list/unit-of-measure-list.component';
 import { UnitOfMeasureCreationComponent } from './components/unit-of-measure-creation/unit-of-measure-creation.component';
 import { UnitOfMeasureEditComponent } from './components/unit-of-measure-edit/unit-of-measure-edit.component';
+import { CategoryEditComponent } from './components/category-edit/category-edit.component';
 const routes: Routes = [
   { path: 'product-creation', component: ProductCreationComponent },
   { path: 'product-list', component: ProductListComponent },
   { path: 'product-edit/:id', component: ProductEditComponent },
   { path: 'category-list', component: CategoryListComponent},
   { path: 'category-creation', component: CategoryCreationComponent},
-  { path: 'category-edit/:id', component: CategoryCreationComponent},
+  { path: 'category-edit/:id', component: CategoryEditComponent},
   { path: 'unitOfMeasure-list', component: UnitOfMeasureListComponent},
   { path: 'unitOfMeasure-creation', component: UnitOfMeasureCreationComponent},
   { path: 'unitOfMeasure-edit', component: UnitOfMeasureEditComponent}
