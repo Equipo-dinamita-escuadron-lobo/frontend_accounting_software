a3c70631ff1f926b48f39892d9d26faeda6c8058
rscruz <santiagocrzzz1711@gmail.com>
2024-04-19 15:56:26 -0500

    Adding chart account module

---
+++ src/app/modules/commercial/chart-accounts/chart-accounts.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ChartAccountsRoutingModule } from './chart-accounts-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ChartAccountsRoutingModule
+  ]
+})
+export class ChartAccountsModule { }
