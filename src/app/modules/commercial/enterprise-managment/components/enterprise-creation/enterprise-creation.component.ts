0ab5577826f9e762b2da708d6fb7d29e6960be79
rscruz <santiagocrzzz1711@gmail.com>
2024-04-19 13:46:13 -0500

    fixing restrictions form enterprise

--- src/app/modules/commercial/enterprise-managment/components/enterprise-creation/enterprise-creation.component.ts
+++ src/app/modules/commercial/enterprise-managment/components/enterprise-creation/enterprise-creation.component.ts
@@ -109,7 +109,7 @@ export class EnterpriseCreationComponent {
         [
           Validators.required,
           Validators.maxLength(50),
-          Validators.pattern(/^[a-zA-Z\s]+$/),
+          Validators.pattern(/^[a-zA-Z0-9\s\-.,&()']+$/),
         ],
       ],
       nit: [
@@ -129,7 +129,14 @@ export class EnterpriseCreationComponent {
           Validators.pattern(/^\d+$/),
         ],
       ],
-      email: ['', [Validators.required, Validators.email]],
+      email: [
+        '',
+        [
+          Validators.required,
+          Validators.email,
+          Validators.pattern(/^[a-zA-Z0-9\s\-.]+$/),
+        ],
+      ],
       country: [
         { value: 'Colombia', disabled: true },
         [Validators.maxLength(50)],
