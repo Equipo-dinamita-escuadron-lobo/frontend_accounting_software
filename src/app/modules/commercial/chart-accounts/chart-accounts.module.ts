552922c99cd12c47859030057dcbd3bb42cf12db
nimbachi <nimbachi@unicauca.edu.co>
2024-04-20 16:28:37 -0500

    create-list-accounts-component

--- src/app/modules/commercial/chart-accounts/chart-accounts.module.ts
+++ src/app/modules/commercial/chart-accounts/chart-accounts.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ChartAccountsRoutingModule } from './chart-accounts-routing.module';
+import { AccountsListComponent } from './components/accounts-list/accounts-list.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    AccountsListComponent
+  ],
   imports: [
     CommonModule,
     ChartAccountsRoutingModule
