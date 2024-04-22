c797bbd716aa21872c8eca3a5827cacbf3bc71a0
AlejFernandezV <alejfernandez@unicauca.edu.co>
2024-04-22 18:56:32 -0500

    Adding actions in third parties list and test of login component

--- src/app/modules/principal/login/components/login-view/login-view.component.spec.ts
+++ src/app/modules/principal/login/components/login-view/login-view.component.spec.ts
@@ -1,23 +1,44 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 import { LoginViewComponent } from './login-view.component';
+
+// Mock para el Router
+class RouterStub {
+  navigate(commands: any[], extras?: any) { }
+}
 
 describe('LoginViewComponent', () => {
   let component: LoginViewComponent;
   let fixture: ComponentFixture<LoginViewComponent>;
+  let router: Router;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [LoginViewComponent]
-    })
-    .compileComponents();
-    
+      imports: [FormsModule],
+      declarations: [LoginViewComponent],
+      providers: [
+        { provide: Router, useClass: RouterStub }
+      ]
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
     fixture = TestBed.createComponent(LoginViewComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    router = TestBed.inject(Router); // Inyecta el Router en tu test
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should navigate to "/general/" on login', () => {
+    const spy = spyOn(router, 'navigate');
+    component.email = 'test@example.com';
+    component.password = 'password';
+    component.login();
+    expect(spy).toHaveBeenCalledWith(['/general/']);
   });
 });
