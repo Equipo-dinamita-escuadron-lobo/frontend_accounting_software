4bcd7be80010639d9f55fc6e5ee070d76fe840d7
rscruz <santiagocrzzz1711@gmail.com>
2024-05-30 00:12:49 -0500

    add log out

--- src/app/shared/components/header-enterprises/header-enterprises.component.ts
+++ src/app/shared/components/header-enterprises/header-enterprises.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../../modules/principal/login/services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header-enterprises',
@@ -6,5 +8,20 @@ import { Component } from '@angular/core';
   styleUrl: './header-enterprises.component.css'
 })
 export class HeaderEnterprisesComponent {
+
+  constructor(private authService: AuthService,
+    private router: Router
+  ){
+
+  }
+
+  logOut(){
+    this.authService.logout();
+    this.logInUser();
+  }
+
+  logInUser() {
+    this.router.navigate(['/']);
+  }
 
 }
