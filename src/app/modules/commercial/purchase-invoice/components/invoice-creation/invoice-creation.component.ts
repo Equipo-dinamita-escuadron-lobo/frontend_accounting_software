19b04e600b2b6414ec38e1317a1e8a097e52e183
nimbachi <nimbachi@unicauca.edu.co>
2024-05-30 11:31:49 -0500

    adding-purchase-module

---
+++ src/app/modules/commercial/purchase-invoice/components/invoice-creation/invoice-creation.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-invoice-creation',
+  templateUrl: './invoice-creation.component.html',
+  styleUrl: './invoice-creation.component.css'
+})
+export class InvoiceCreationComponent {
+
+}
