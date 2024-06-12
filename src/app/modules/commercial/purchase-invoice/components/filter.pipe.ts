70a318d87b7a210dfafdf7d4c8b52af41f5d9b97
nimbachi <nimbachi@unicauca.edu.co>
2024-06-11 23:57:59 -0500

    adding supplier filter

--- src/app/modules/commercial/purchase-invoice/components/filter.pipe.ts
+++ src/app/modules/commercial/purchase-invoice/components/filter.pipe.ts
@@ -1,12 +1,20 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'filter'
+  name: 'filterThird'
 })
 export class FilterPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(items: any[], filterList: string): any[] {
+    if ((!filterList || filterList.trim() === '')) {
+      return items;
+    }
+
+    return items.filter(item => item.names.toLowerCase().includes(filterList.toLowerCase())
+      || item.socialReason.toLowerCase().includes(filterList.toLowerCase())
+      || item.idNumber.toString().includes(filterList)
+      || item.typeId.toString().toLowerCase().includes(filterList.toLowerCase())
+      || item.email.toLowerCase().includes(filterList.toLowerCase()));
   }
 
 }
--- src/app/modules/commercial/purchase-invoice/components/invoice-select-supplier/invoice-select-supplier.component.ts
+++ src/app/modules/commercial/purchase-invoice/components/invoice-select-supplier/invoice-select-supplier.component.ts
@@ -1,8 +1,8 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { ThirdServiceService } from '../../../third-parties-managment/services/third-service.service';
-import { FormBuilder, FormGroup, FormControl} from '@angular/forms';
 import Swal from 'sweetalert2';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-invoice-select-supplier',
