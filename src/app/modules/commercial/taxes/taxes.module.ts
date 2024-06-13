f1bb3dbbc2b7841431860c8818338880322784e8
Juliana2101 <128009669+Juliana2101@users.noreply.github.com>
2024-06-13 08:15:20 -0500

    fix ng declaration on taxes module

--- src/app/modules/commercial/taxes/taxes.module.ts
+++ src/app/modules/commercial/taxes/taxes.module.ts
@@ -1,8 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { TaxesRoutingModule } from './taxes-routing.module';
-import { NombreDelComponenteComponent } from './components/nombre-del-componente/nombre-del-componente.component';
 import { CreateTaxComponent } from './components/create-tax/create-tax.component';
 import { ListTaxComponent } from './components/list-tax/list-tax.component';
 import { EditTaxComponent } from './components/edit-tax/edit-tax.component';
@@ -10,7 +8,6 @@ import { EditTaxComponent } from './components/edit-tax/edit-tax.component';
 
 @NgModule({
   declarations: [
-    NombreDelComponenteComponent,
     CreateTaxComponent,
     ListTaxComponent,
     EditTaxComponent
